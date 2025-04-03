806dc91a99b6914a6fd3ad93722fc2265c649141
klxcoder <klxcoder@gmail.com>
2025-04-03 09:24:24 +0700

    plot fail and pass

--- week3/08.py
+++ week3/08.py
@@ -1,9 +1,27 @@
 import matplotlib.pyplot as plt
 
+def get_data():
+    xs = [[1, 1], [2, 2]]
+    ys = [0, 1]
+    return xs, ys
+
 def main():
+    xs, ys = get_data()
+
     _, axes = plt.subplots(1, 2, figsize=(10, 5))
 
     # Plot the data on the left subplot
+    fail_label_added = False
+    pass_label_added = False
+
+    for i in range(len(xs)):
+        if ys[i] == 0:
+            axes[0].scatter(xs[i][0], xs[i][1], marker='o', color='red', label='Fail' if not fail_label_added else "")
+            fail_label_added = True
+        else:
+            axes[0].scatter(xs[i][0], xs[i][1], marker='s', color='blue', label='Pass' if not pass_label_added else "")
+            pass_label_added = True
+
     axes[0].set_title('data')
     axes[0].set_xlabel('x1 (hours of study)')
     axes[0].set_ylabel('x2 (hours of sleep)')
